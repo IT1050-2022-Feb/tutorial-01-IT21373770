d078439e246e4e207290f517035d581c966b8af3
udayanga <99312313+IT21373770@users.noreply.github.com>
2022-02-09 06:06:41 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2;
+  float AVG;
+
+  printf("Enter mark 01:");
+  scanf("%d",&mark1);
+
+  printf("Enter mark 02:");
+  scanf("%d",&mark2);
+
+  AVG = mark1+mark2/2.0;
   
+  printf("The average is:%.2f",AVG);
   return 0;
 }
 
